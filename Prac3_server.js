0bc8be75ef3df8225a6d9502d24c2f20afb8b5b7
MD KAVIUL HOSSAIN <kaviuln@gmail.com>
2022-08-16 12:12:09 +1000

    Final Update

--- Prac3_server.js
+++ Prac3_server.js
@@ -1,11 +1,15 @@
-var express = require("express");
-var app = express();
+var express = require("express")
+var app = express()
 
 app.use(express.static(__dirname+'/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 var port = process.env.port || 8080;
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname+'/public/Prac3_index.html');
+})
 
 app.listen(port,()=>{
     console.log("App listening to: " + port)
